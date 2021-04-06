24a64b58d64851fca54622ea5b00d916e84729fb
josh-desktop <rcxwhiz@gmail.com>
2021-04-06 13:45:11 -0600

    Fixed the coagulation model none test

--- test/standard/CoagulationModel_NONE.cpp
+++ test/standard/CoagulationModel_NONE.cpp
@@ -4,16 +4,16 @@
 
 #include "TestTools.h"
 
+#include "sootlib/coagulation_model/CoagulationModel_NONE.h"
+
 TEST (CoagulationModel_NONE_TEST, basic_test)
 {
-	const soot::State gasState = TestTools::getBlankGasState();
-	const soot::MomentSootState sootState = TestTools::getBlankMomentSootState();
-
-	const double expectedValue = 0.0;
-	std::unique_ptr<soot::CoagulationModel> model = std::make_unique<soot::CoagulationModel_NONE>();
+	const InputState inputState = TestTools::getBlankState();
+	unique_ptr<CoagulationModel> model = make_unique<CoagulationModel_NONE>();
 
 	// Using exact comparison since model should return exactly 0
-	EXPECT_EQ (expectedValue, model->getCoagulationRate(gasState, sootState, 0, 0));
+    const double expectedValue = 0.0;
+    EXPECT_EQ (expectedValue, model->getCoagulationRate(inputState, 0, 0));
 }
 
 int main(int argc, char** argv)
--- test/testtools/TestTools.h
+++ test/testtools/TestTools.h
@@ -1,68 +1,16 @@
 #ifndef TESTTOOLS_H
 #define TESTTOOLS_H
 
-#include <memory>
+#include "sootlib/state/InputState.h"
 
-#include "sootlib/state/gas/State.h"
-#include "sootlib/state/soot/MomentSootState.h"
-#include "sootlib/state/soot/BinSootState.h"
-#include "sootlib/soot_models/bin/BinSootModel.h"
-#include "sootlib/soot_model/generator/BinSootModelGenerator.h"
-#include "sootlib/soot_models/moment/MomentSootModel.h"
-#include "sootlib/soot_model/generator/MomentSootModelGenerator.h"
+using namespace soot;
+using namespace std;
 
 class TestTools
 {
 public:
-	[[nodiscard]] static soot::State getBlankGasState()
-	{
-		return soot::State();
-	}
-
-	[[nodiscard]] static soot::MomentSootState getBlankMomentSootState()
-	{
-		return soot::MomentSootState();
-	}
-
-	[[nodiscard]] static soot::BinSootState getBlankBinSootState()
-	{
-		return soot::BinSootState();
-	}
-	
-	[[nodiscard]] static soot::BinSootModel* getDefaultBinSootModel()
-	{
-		auto generator = soot::BinSootModelGenerator();
-		return generator.getModel();
-	}
-	
-	[[nodiscard]] static std::unique_ptr<soot::BinSootModel> getDefaultBinSootModelUnique()
-	{
-		auto generator = soot::BinSootModelGenerator();
-		return generator.getModelUnique();
-	}
-	
-	[[nodiscard]] static std::shared_ptr<soot::BinSootModel> getDefaultBinSootModelShared()
-	{
-		auto generator = soot::BinSootModelGenerator();
-		return generator.getModelShared();
-	}
-
-	[[nodiscard]] static soot::SootModel* getDefaultMomentSootModel()
-	{
-		auto generator = soot::MomentSootModelGenerator();
-		return generator.getModel();
-	}
-
-	[[nodiscard]] static std::unique_ptr<soot::SootModel> getDefaultMomentSootModelUnique()
-	{
-		auto generator = soot::MomentSootModelGenerator();
-		return generator.getModelUnique();
-	}
-
-	[[nodiscard]] static std::shared_ptr<soot::SootModel> getDefaultMomentSootModelShared()
-	{
-		auto generator = soot::MomentSootModelGenerator();
-		return generator.getModelShared();
+	[[nodiscard]] static InputState getBlankState() {
+	    return InputState();
 	}
 };
 
