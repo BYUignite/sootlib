af9336ab6617411c71e7c553f106aa0b18faedcf
vbstephens <victoria.b.stephens@gmail.com>
2021-08-18 15:01:05 -0500

    Added oxidation mechanism tests

--- tests/oxidationModelTests.cc
+++ tests/oxidationModelTests.cc
@@ -61,6 +61,78 @@ TEST_CASE("oxidation model initialization", "[oxidation][init]") {
         REQUIRE(SM.psd->oxi->oxidationRxnRatios.at(gasSp::O2) == -0.5);
         REQUIRE(SM.psd->oxi->oxidationRxnRatios.at(gasSp::C) == -2);
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("getOxidationSootRate function call", "[groth][getSootRate]") {
+
+    // NOTE: using 'bad' (unphysical) values should throw errors in the setState function;
+    // those tests are covered in stateTests.cc
+
+    oxidationMech NONE = oxidationMech::NONE;
+    oxidationMech LL   = oxidationMech::LL;
+    oxidationMech HACA = oxidationMech::HACA;
+    oxidationMech LEE  = oxidationMech::LEE_NEOH;
+    oxidationMech NSC  = oxidationMech::NSC_NEOH;
+
+    nucleationMech  n = nucleationMech::NONE;
+    growthMech      g = growthMech::NONE;
+    coagulationMech c = coagulationMech::NONE;
+
+    vector<double> yGas = {0, 0.1, 0.1, 0.01, 0.02, 0.03, 0.04, 2E-5};        // [H, H2, O, O2, OH, H2O, CO, C2H2]
+    vector<double> yPAH = {0, 0.001, 0.001, 0.001, 0.0002, 0};                 // [C10H8, C12H8, C12H10, C14H10, C16H10, C18H10]
+    vector<double> ySootVar{1E14, 1E-12};
+
+    state S = state();
+    S.setState(2100, 101325, 0.1, 1E-5, 29, yGas, yPAH, ySootVar);
+
+    SECTION("NONE oxidation model", "[NONE][oxidation][getSootRate]") {
+
+        sootModel SM = sootModel(psdMech::MONO, 2, n, g, NONE, c);
+        double oxiSootRate = SM.psd->oxi->getOxidationSootRate(S);
+        REQUIRE(oxiSootRate == 0);
+    }
+
+    SECTION("LL oxidation model", "[LL][oxidation][getSootRate]") {
+
+        sootModel SM = sootModel(psdMech::MONO, 2, n, g, LL, c);
+        double oxiSootRate = SM.psd->oxi->getOxidationSootRate(S);
+
+        // requires that the result is on the order of 1E-5
+        REQUIRE(oxiSootRate/1E-5 < 10.0);
+        REQUIRE(oxiSootRate/1E-5 > 1.0);
+    }
+
+    SECTION("HACA oxidation model", "[HACA][oxidation][getSootRate]") {
+
+        sootModel SM = sootModel(psdMech::MONO, 2, n, g, HACA, c);
+        double oxiSootRate = SM.psd->oxi->getOxidationSootRate(S);
+
+        // requires that the result is on the order of 0.1
+        REQUIRE(oxiSootRate/1E-1 < 10.0);
+        REQUIRE(oxiSootRate/1E-1 > 1.0);
+    }
+
+    SECTION("LEE_NEOH oxidation model", "[LEE_NEOH][oxidation][getSootRate]") {
+
+        sootModel SM = sootModel(psdMech::MONO, 2, n, g, LEE, c);
+        double oxiSootRate = SM.psd->oxi->getOxidationSootRate(S);
+
+        // requires that the result is on the order of 0.1
+        REQUIRE(oxiSootRate/1E-1 < 10.0);
+        REQUIRE(oxiSootRate/1E-1 > 1.0);
+    }
+
+    SECTION("NSC_NEOH oxidation model", "[NSC_NEOH][oxidation][getSootRate]") {
+
+        sootModel SM = sootModel(psdMech::MONO, 2, n, g, NSC, c);
+        double oxiSootRate = SM.psd->oxi->getOxidationSootRate(S);
+
+        // requires that the result is on the order of 0.1
+        REQUIRE(oxiSootRate/1E-1 < 10.0);
+        REQUIRE(oxiSootRate/1E-1 > 1.0);
+    }
 
 }
 
