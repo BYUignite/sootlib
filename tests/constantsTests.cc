c3ce0e070bc6fa494799f82f2398da753a35e7a1
vbstephens <victoria.b.stephens@gmail.com>
2021-09-27 13:10:04 -0500

    Added tests for setting and retrieving values of a sourceTermStruct object.

--- tests/constantsTests.cc
+++ tests/constantsTests.cc
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace soot;
 
+////////////////////////////////////////////////////////////////////////////////
+
 TEST_CASE("check for valid constants values", "[constants]") {
 
     SECTION("true constants are unchanged") {
@@ -21,5 +23,59 @@ TEST_CASE("check for valid constants values", "[constants]") {
         REQUIRE( bCoag >= 1/sqrt(2) );
         REQUIRE( bCoag <= 1 );
     }
-
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("get and set values from sourceTermStruct object", "[sourceTerms]") {
+
+    sourceTermStruct ST;
+
+    SECTION("check initialization values") {
+
+        // soot source terms
+        REQUIRE(ST.sootSourceTerms.at(0) == 0);
+        REQUIRE(ST.sootSourceTerms.at(1) == 0);
+
+        // gas source terms
+        for (auto const& x : ST.gasSourceTerms) {
+            gasSp sp = x.first;
+            REQUIRE(ST.gasSourceTerms.at(sp) == 0);
+        }
+
+        // pah source terms
+        for (auto const& x : ST.pahSourceTerms) {
+            pahSp sp = x.first;
+            REQUIRE(ST.pahSourceTerms.at(sp) == 0);
+        }
+    }
+
+    SECTION("set values and check retrieval") {
+
+        // soot source terms
+        ST.sootSourceTerms.at(0) = 2.1;
+        ST.sootSourceTerms.at(1) = 1.1E-5;
+
+        REQUIRE(ST.sootSourceTerms.at(0) == 2.1);
+        REQUIRE(ST.sootSourceTerms.at(1) == 1.1E-5);
+
+        // gas source terms
+        for (auto const& x : ST.gasSourceTerms) {
+            gasSp sp = x.first;
+            double d = 1.1;
+            ST.gasSourceTerms.at(sp) = d;
+            REQUIRE(ST.gasSourceTerms.at(sp) == d);
+            d = d + 1;
+        }
+
+        // pah source terms
+        for (auto const& x : ST.pahSourceTerms) {
+            pahSp sp = x.first;
+            double d = 1.1E-5;
+            ST.pahSourceTerms.at(sp) = d;
+            REQUIRE(ST.pahSourceTerms.at(sp) == d);
+            d = d*10;
+        }
+    }
+}
+
