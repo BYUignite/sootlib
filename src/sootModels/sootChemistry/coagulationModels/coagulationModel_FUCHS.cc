844c18d83a9e570c42d28afb62268638dd2a0c1d
vbstephens <victoria.b.stephens@gmail.com>
2022-09-05 12:36:42 -0500

    Bugfix: coagulation models now return 0 rate if either mass input is zero

--- src/sootModels/sootChemistry/coagulationModels/coagulationModel_FUCHS.cc
+++ src/sootModels/sootChemistry/coagulationModels/coagulationModel_FUCHS.cc
@@ -7,6 +7,9 @@ double coagulationModel_FUCHS::getCoagulationSootRate(const state& state,
                                                       double m1,
                                                       double m2) const {
 
+    if (m1 == 0 || m2 == 0)
+        return 0;
+
     double Dp1 = pow(6 * abs(m1) / M_PI / rhoSoot, 1.0 / 3);
     double Dp2 = pow(6 * abs(m2) / M_PI / rhoSoot, 1.0 / 3);
 
--- src/sootModels/sootChemistry/coagulationModels/coagulationModel_LL.cc
+++ src/sootModels/sootChemistry/coagulationModels/coagulationModel_LL.cc
@@ -7,6 +7,9 @@ double coagulationModel_LL::getCoagulationSootRate(const state& state,
                                                    double m1,
                                                    double m2) const {
 
+    if (m1 == 0 || m2 == 0)
+        return 0;
+
     double Ca = 9;
     double Dp1 = pow(6 * abs(m1) / M_PI / rhoSoot, 1.0 / 3);
     return 2 * Ca * sqrt(Dp1 * 6 * kb * state.T / rhoSoot);
