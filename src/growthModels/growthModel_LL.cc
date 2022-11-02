47e817281106dea26eecfd2fecdbe2bfc77fb49c
David Lignell <dol4@fuego.et.byu.edu>
2022-11-02 16:50:06 -0600

    generalizing growthModel_LL to sectional

--- src/growthModels/growthModel_LL.cc
+++ src/growthModels/growthModel_LL.cc
@@ -20,7 +20,15 @@ double growthModel_LL::getGrowthSootRate(const state &state) const {
 
     double M0 = state.sootVar[0];
     double M1 = state.sootVar[1];
-    // todo: generalize this to sectional (?)
+
+    if (SM->psdMechType == psdMech::SECT) {
+        M0 = 0.0;
+        M1 = 0.0;
+        for (size_t k=0; k<state.nsoot; k++) {
+            M0 += state.sootVar[k];
+            M1 += state.sootVar[k]*SM->mBins[k];
+        }
+    }
 
     if (M0 > 0)
         Am2m3 = M_PI * pow(abs(6 / (M_PI * rhoSoot) * M1 / M0), 2.0/3.0) * M0;
