BYUignite/sootlib

commit 59f244d1be260310f55e29cc1f778d1f46842473

... documentation updates to source files, not done yet

src/growthModels/growthModel.h
@@ -8,19 +8,24 @@
 namespace soot {
 class sootModel;
 
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Abstract base class to calculate growth rate
+///
+///////////////////////////////////////////////////////////////////////////////
+
 class growthModel {
 
-//////////////// DATA MEMBERS /////////////////////
+    //////////////// DATA MEMBERS /////////////////////
 
 public:
 
-    std::vector<double> growthRxnRatios;
+    sootModel *SM;                        ///< back pointer to soot model
+    coagulationMech mechType;             ///< identity of the type of growth (child)
 
-    growthMech mechType;
+    std::vector<double> growthRxnRatios;  ///< mole ratios for gas species rate coupling
 
-    sootModel *SM;
-
-//////////////// MEMBER FUNCTIONS /////////////////
+    //////////////// MEMBER FUNCTIONS /////////////////
 
     virtual double getGrowthSootRate(const state& state) const = 0;
 
@@ -28,9 +33,7 @@ public:
                            const double &msootDotGrow, 
                            std::vector<double> &gasSourcesGrow) const;
 
-//        virtual void getGrowthPahRates(state& state) const = 0;
-
-//////////////// CONSTRUCTOR FUNCTIONS ////////////
+    //////////////// CONSTRUCTOR FUNCTIONS ////////////
 
     growthModel() : growthRxnRatios(std::vector<double>((size_t)gasSp::size, 0.0)) { }
     virtual ~growthModel() = default;

src/nucleationModels/nucleationModel.h
@@ -9,22 +9,27 @@ namespace soot {
 
 class sootModel;
 
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Abstract base class to calculate nucleation rates
+///
+///////////////////////////////////////////////////////////////////////////////
+
 class nucleationModel {
 
-//////////////// DATA MEMBERS /////////////////////
+    //////////////// DATA MEMBERS /////////////////////
 
 public:
 
-    dimerStruct DIMER = dimerStruct();      // used for PAH nucleation only
+    sootModel *SM;                              ///< back pointer to soot model
+    coagulationMech mechType;                   ///< identity of the type of coagulation (child)
 
-    std::vector<double> nucleationRxnRatios;
-    std::vector<double> nucleationPahRxnRates;
+    dimerStruct DIMER = dimerStruct();          ///< used for PAH nucleation only
 
-    nucleationMech mechType;
+    std::vector<double> nucleationRxnRatios;    ///< mole ratios for gas species rate coupling
+    std::vector<double> nucleationPahRxnRates;  ///< mole ratios for PAH gas species rate coupling
 
-    sootModel *SM;
-
-//////////////// MEMBER FUNCTIONS /////////////////
+    //////////////// MEMBER FUNCTIONS /////////////////
 
     virtual double getNucleationSootRate(state &state) = 0;
 
@@ -32,7 +37,7 @@ public:
                                const double &msootDotNucl, 
                                std::vector<double> &gasSourcesNucl) const;
 
-//////////////// CONSTRUCTOR FUNCTIONS ////////////
+    //////////////// CONSTRUCTOR FUNCTIONS ////////////
 
     nucleationModel() : nucleationPahRxnRates(std::vector<double>((int)pahSp::size, 0.0)),
                         nucleationRxnRatios(  std::vector<double>((int)gasSp::size, 0.0)) { }

src/oxidationModels/oxidationModel.h
@@ -9,19 +9,24 @@ namespace soot {
 
 class sootModel;
 
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Abstract base class to calculate oxidation rates
+///
+///////////////////////////////////////////////////////////////////////////////
+
 class oxidationModel {
 
-//////////////// DATA MEMBERS /////////////////////
+    //////////////// DATA MEMBERS /////////////////////
 
 public:
 
-    std::vector<double> oxidationRxnRatios;
+    sootModel *SM;                           ///< back pointer to soot model
+    coagulationMech mechType;                ///< identity of the type of coagulation (child)
 
-    oxidationMech mechType;
+    std::vector<double> oxidationRxnRatios;  ///< mole ratios for gas species rate coupling
 
-    sootModel *SM;
-
-//////////////// MEMBER FUNCTIONS /////////////////
+    //////////////// MEMBER FUNCTIONS /////////////////
 
     virtual double getOxidationSootRate(const state &state) const = 0;
 
@@ -29,9 +34,7 @@ public:
                                const double &msootDotOxid, 
                                std::vector<double> &gasSourcesOxid) const;
 
-//        virtual void getOxidationPahRates(state& state) const = 0;
-
-//////////////// CONSTRUCTOR FUNCTIONS ////////////
+    //////////////// CONSTRUCTOR FUNCTIONS ////////////
 
     oxidationModel() : oxidationRxnRatios(std::vector<double>((int)gasSp::size, 0.0)) {
         oxidationRxnRatios[(int)gasSp::C] = 1.0;

src/coagulationModels/coagulationModel.h
@@ -6,36 +6,29 @@ namespace soot {
 
 class sootModel;
 
-/**
- * Interface used to calculate coagulation rate given a state and two masses
- */
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Abstract base class to calculate coagulation rate given a state and two masses.
+///
+///////////////////////////////////////////////////////////////////////////////
+
 class coagulationModel {
 
     //////////////// DATA MEMBERS /////////////////////
 
 public:
 
-    sootModel *SM;
-
-    coagulationMech mechType;
+    sootModel *SM;                ///< back pointer to soot model
+    coagulationMech mechType;     ///< identity of the type of coagulation (child)
 
 protected:
 
-    double FM_multiplier;         // multiply the FM rate by this constant (1.0 default, user set)
+    double FM_multiplier;         ///< multiply the FM rate by this constant (1.0 default, user set)
 
     //////////////// MEMBER FUNCTIONS /////////////////
 
 public:
 
-    /**
-     * Unimplemented function to calculate a coagulation rate with the given parameters, not all of which might be used depending on the
-     * implementation
-     *
-     * @param state
-     * @param m1
-     * @param m2
-     * @return
-     */
     virtual double getCoagulationSootRate(const state& state, double m1, double m2) const = 0;
 
     void set_FM_multiplier(double FM_multiplier_) {

src/coagulationModels/coagulationModel_CONTINUUM.cc
@@ -4,6 +4,17 @@
 using namespace std;
 using namespace soot;
 
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Calculates coagulation rate - required by coagulationModel.
+///
+/// @param state \input       gas and soot state, set by user.
+/// @param m1    \input
+/// @param m2    \input
+/// @return coagulation rate (\#/m3*s)
+///
+///////////////////////////////////////////////////////////////////////////////
+
 double coagulationModel_CONTINUUM::getCoagulationSootRate(const state& state,
                                                           double m1,
                                                           double m2) const {

src/coagulationModels/coagulationModel_CONTINUUM.h
@@ -6,19 +6,19 @@
 namespace soot {
 
 ////////////////////////////////////////////////////////////////////////////////
-/*! Coagulation: Continuum Regime
- *
- *      Implementation of coagulationModel interface
- *
- *      Returns the value of the collision rate function (beta, or K12) (=) m3/#*s.
+///
+/// Coagulation: Continuum Regime
+/// Implementation of coagulationModel interface
+///
+/// From Seinfeld and Pandis Atmospheric Chemistry book (2016), pg. 547, chp 13.
+///
+/// @param MomentState      \input      local moment state; need M0 and M1
+/// @param m1               \input      first particle size (kg)
+/// @param m2               \input      second particle size (kg)
+/// @return value of the collion rate function (beta12 or K12) (=) m3/\#*s
+///
+////////////////////////////////////////////////////////////////////////////////
 
- *      From Seinfeld and Pandis Atmospheric Chemistry book (2016), pg. 547, chp 13.
- *
- *      @param MomentState&     \input      local moment state; need M0 and M1
- *      @param m1               \input      first particle size (kg)
- *      @param m2               \input      second particle size (kg)
- *
- */
 class coagulationModel_CONTINUUM : public coagulationModel {
 
     //////////////// DATA MEMBERS /////////////////////
@@ -26,14 +26,7 @@ class coagulationModel_CONTINUUM : public coagulationModel {
     //////////////// MEMBER FUNCTIONS /////////////////
 
 public:
-    /**
-     * Calculates coagulation rate - required by coagulationModel
-     *
-     * @param state
-     * @param m1
-     * @param m2
-     * @return
-     */
+
     double getCoagulationSootRate(const state& state, double m1, double m2) const override;
 
     //////////////// CONSTRUCTOR FUNCTIONS ////////////

src/sootModel.cc
@@ -30,11 +30,11 @@ using namespace soot;
 /// Constructor taking pointers to chemistry models as input.
 /// User creates these pointers nominally by "new-ing" them.
 ///
-/// @param \input nsoot_ number of soot variables (moments or sections)
-/// @param \input nucl_  pointer to nucleation model.
-/// @param \input grow_  pointer to growth model.
-/// @param \input oxid_  pointer to oxidation model.
-/// @param \input coag_  pointer to coagulation model.
+/// @param nsoot_ \input number of soot variables (moments or sections)
+/// @param nucl_  \input pointer to nucleation model.
+/// @param grow_  \input pointer to growth model.
+/// @param oxid_  \input pointer to oxidation model.
+/// @param coag_  \input pointer to coagulation model.
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -58,11 +58,11 @@ sootModel::sootModel(size_t            nsoot_,
 /// Constructor taking enumerations names as input.
 /// Chemistry pointers are created (new-ed) here based on those enumerations.
 ///
-/// @param \input nsoot_ number of soot variables (moments or sections)
-/// @param \input Nmech  one of enum class nucleationMech in sootDefs.h
-/// @param \input Gmech  one of enum class growthMech in sootDefs.h
-/// @param \input Omech  one of enum class oxidationMech in sootDefs.h
-/// @param \input Cmech  one of enum class coagulationMech in sootDefs.h
+/// @param nsoot_ \input number of soot variables (moments or sections)
+/// @param Nmech  \input one of enum class nucleationMech in sootDefs.h
+/// @param Gmech  \input one of enum class growthMech in sootDefs.h
+/// @param Omech  \input one of enum class oxidationMech in sootDefs.h
+/// @param Cmech  \input one of enum class coagulationMech in sootDefs.h
 ///
 ////////////////////////////////////////////////////////////////////////////////
 

src/sootModel_LOGN.cc
@@ -8,11 +8,11 @@ using namespace soot;
 /// Constructor taking pointers to chemistry models as input.
 /// User creates these pointers nominally by "new-ing" them.
 ///
-/// @param \input nsoot_ number of soot moments (should be 3).
-/// @param \input nucl_  pointer to nucleation model.
-/// @param \input grow_  pointer to growth model.
-/// @param \input oxid_  pointer to oxidation model.
-/// @param \input coag_  pointer to coagulation model.
+/// @param nsoot_ \input number of soot moments (should be 3).
+/// @param nucl_  \input pointer to nucleation model.
+/// @param grow_  \input pointer to growth model.
+/// @param oxid_  \input pointer to oxidation model.
+/// @param coag_  \input pointer to coagulation model.
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -34,11 +34,11 @@ sootModel_LOGN::sootModel_LOGN(size_t            nsoot_,
 /// Constructor taking enumerations names as input.
 /// Chemistry pointers are created (new-ed) here based on those enumerations.
 ///
-/// @param \input nsoot_ number of soot moments (should be 3).
-/// @param \input Nmech  one of enum class nucleationMech in sootDefs.h
-/// @param \input Gmech  one of enum class growthMech in sootDefs.h
-/// @param \input Omech  one of enum class oxidationMech in sootDefs.h
-/// @param \input Cmech  one of enum class coagulationMech in sootDefs.h
+/// @param nsoot_ \input number of soot moments (should be 3).
+/// @param Nmech  \input one of enum class nucleationMech in sootDefs.h
+/// @param Gmech  \input one of enum class growthMech in sootDefs.h
+/// @param Omech  \input one of enum class oxidationMech in sootDefs.h
+/// @param Cmech  \input one of enum class coagulationMech in sootDefs.h
 ///
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -64,8 +64,8 @@ sootModel_LOGN::sootModel_LOGN(size_t          nsoot_,
 /// Function only called if nucleationMech::PAH.
 /// Function called by nucleationModel_PAH::getNucleationSootRate
 ///
-/// @param \input state gas and soot state, set by user.
-/// @param \input mDimer dimer mass (kg)
+/// @param state  \input gas and soot state, set by user.
+/// @param mDimer \input  dimer mass (kg)
 /// @return pah/soot sollision rate per dimer. Call it I. I*mDimer*nDimer = Cnd1 (=) kg/m3*s
 ///
 ////////////////////////////////////////////////////////////////////////////////
@@ -125,10 +125,10 @@ double sootModel_LOGN::pahSootCollisionRatePerDimer(const state &state, const do
 ///
 /// Primary user interface.
 /// 
-/// @param \input  state       gas and soot state, set by user.
-/// @param \output sootSources soot moment (r) sources (kg^r/m3*s).
-/// @param \output gasSources  vector of gas species rates (kg/m3*s)
-/// @param \output pahSources  vector of gas PAH species rates (kg/m3*s)
+/// @param state       \input  gas and soot state, set by user.
+/// @param sootSources \output soot moment (r) sources (kg^r/m3*s).
+/// @param gasSources  \output vector of gas species rates (kg/m3*s)
+/// @param pahSources  \output vector of gas PAH species rates (kg/m3*s)
 ///
 ////////////////////////////////////////////////////////////////////////////////
 

commit ec86412dbc740d5841859f8ac39ce9559936b541

fixing minor bug introduced by DOL in 59f244d

src/growthModels/growthModel.h
@@ -20,8 +20,8 @@ class growthModel {
 
 public:
 
-    sootModel *SM;                        ///< back pointer to soot model
-    coagulationMech mechType;             ///< identity of the type of growth (child)
+    sootModel *SM;                 ///< back pointer to soot model
+    growthMech mechType;           ///< identity of the type of growth (child)
 
     std::vector<double> growthRxnRatios;  ///< mole ratios for gas species rate coupling
 

src/nucleationModels/nucleationModel.h
@@ -22,7 +22,7 @@ class nucleationModel {
 public:
 
     sootModel *SM;                              ///< back pointer to soot model
-    coagulationMech mechType;                   ///< identity of the type of coagulation (child)
+    nucleationMech mechType;                    ///< identity of the type of nucleation (child)
 
     dimerStruct DIMER = dimerStruct();          ///< used for PAH nucleation only
 

src/oxidationModels/oxidationModel.h
@@ -22,7 +22,7 @@ class oxidationModel {
 public:
 
     sootModel *SM;                           ///< back pointer to soot model
-    coagulationMech mechType;                ///< identity of the type of coagulation (child)
+    oxidationMech mechType;                  ///< identity of the type of oxidation (child)
 
     std::vector<double> oxidationRxnRatios;  ///< mole ratios for gas species rate coupling
 