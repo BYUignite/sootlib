aef75779a8d84eefcf7f00fe255b616e1c614245
humedm7 <davishume@gmail.com>
2026-03-06 12:13:33 -0700

    The diameter d2 is calculated using m1 instead of m2.

--- src/coagulationModels/coagulationModel_FUCHS.cc
+++ src/coagulationModels/coagulationModel_FUCHS.cc
@@ -30,7 +30,7 @@ double coagulationModel_FUCHS::getCoagulationSootRate(const state& state,
     if (m1 <= mmin || m2 <= mmin) return 0.0;
 
     double d1 = pow(6.*m1/(M_PI*rhoSoot), onethird);
-    double d2 = pow(6.*m1/(M_PI*rhoSoot), onethird);
+    double d2 = pow(6.*m2/(M_PI*rhoSoot), onethird);
 
     double mfp = state.getGasMeanFreePath();
     double Kn1 = 2*mfp/d1;
